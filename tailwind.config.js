07044111616bcf998d109eeaf75e8c7c6a2e4a74
Hashen Udara <106765093+HashenUdara@users.noreply.github.com>
2023-10-31 22:26:59 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'daily-dev-tips': "#F89283"
+        'brand': "#2e16e6"
       }
     },
+    
+    fontFamily: {
+      sans: ['Mona Sans', 'Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      display: [
+        ['Mona Sans', ...defaultTheme.fontFamily.sans],
+        { fontVariationSettings: '"wdth" 125' },
+      ],
+    },
+
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
